d73fd59e861747460b8080f99285b695e6050662
TreeBot <33048684+Treerach@users.noreply.github.com>
2017-10-26 16:33:29 +0700

    Update treebot3.py

--- treebot3.py
+++ treebot3.py
@@ -64,6 +64,7 @@ helpMessage ="""
 ¤ [Groups]
 ¤ [Cancel]
 ¤ [Clean]
+¤ [Tagall] Mention All User
 ¤ [Invite [mid]]Invite by mid people
 ¤ [Gn "the group name"]
 ¤ [Gurl]
@@ -1688,8 +1689,35 @@ def bot(op):
                                     cl.sendText(msg.to,"ヽ( ^ω^)ﾉ Success")
                                 except:
                                     cl.sendText(msg.to,"error")
-#-----------------------------------------------------------
-#-------------------------------------------------------------------蹴り返し
+#------------------------------------------------#
+#-------------Fungsi Tag All Start---------------#
+            elif msg.text in ["Tr","tr","แทก"]:
+                  group = cl.getGroup(msg.to)
+                  nama = [contact.mid for contact in group.members]
+
+                  cb = ""
+                  cb2 = ""
+                  strt = int(0)
+                  akh = int(0)
+                  for md in nama:
+                      akh = akh + int(6)
+
+                      cb += """{"S":"""+json.dumps(str(strt))+""","E":"""+json.dumps(str(akh))+""","M":"""+json.dumps(md)+"},"""
+
+                      strt = strt + int(7)
+                      akh = akh + 1
+                      cb2 += "@nrik \n"
+
+                  cb = (cb[:int(len(cb)-1)])
+                  msg.contentType = 0
+                  msg.text = cb2
+                  msg.contentMetadata ={'MENTION':'{"MENTIONEES":['+cb+']}','EMTVER':'4'}
+
+                  try:
+                      cl.sendMessage(msg)
+                  except Exception as error:
+                      print error
+#-------------------------------------------------------------------#
         if op.type == 19:
             try:
                 if op.param3 in mid:
