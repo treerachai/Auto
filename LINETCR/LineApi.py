b94c3f4546f779d0bbb6bb9fdfc448dcb7cf8ab3
TreeBot <32605493+treerachai@users.noreply.github.com>
2017-10-30 02:46:05 +0700

    Update LineApi.py

--- LINETCR/LineApi.py
+++ LINETCR/LineApi.py
@@ -311,7 +311,7 @@ class LINE:
 
       prof = self.getProfile()
 
-      print("R.A-BOT")
+      print("Ŧяәәƅoŧ")
       print("mid -> " + prof.mid)
       print("name -> " + prof.displayName)
       print("authToken -> " + self.authToken)
