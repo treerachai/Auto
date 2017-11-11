7ac3c4d13dd2a5e733c19ceac96b7c03c886b5b4
TreeMaster <nutech@outlook.com>
2017-11-12 01:58:28 +0700

    Add files via upload

--- like.py
+++ like.py
@@ -4,21 +4,24 @@ from LINETCR.lib.curve.ttypes import *
 from datetime import datetime
 import time,random,sys,json,codecs,threading,glob,re,base64
 cl = LINETCR.LINE()
+print u"""login start"""
 cl.login(qr=True)
 cl.loginResult()
-ks = ki = kk = kc = cl 
 print u"login success"
 reload(sys)
 sys.setdefaultencoding('utf-8')
+print u"login success"
 i = 0
-c_text = """Auto Like"""
+c_text = """Auto like"""
+#------------------------
 while True:
     try:
         for posts in cl.activity(1)["result"]["posts"]:
             if posts["postInfo"]["liked"] is False:
-                cl.like(posts["userInfo"]["writerMid"], posts["postInfo"]["postId"], 1002)
+                cl.like(posts["userInfo"]["writerMid"], posts["postInfo"]["postId"], 1001)
                 cl.comment(posts["userInfo"]["writerMid"],posts["postInfo"]["postId"],c_text)
                 print u"liked" + str(i)
                 i += 1
     except Exception as e:
             print e
+#------------------------
